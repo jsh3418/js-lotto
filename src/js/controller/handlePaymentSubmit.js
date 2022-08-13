ae66e8f4b1e87ab0e8c27e138cf8987fd8363a05
jsh3418 <albert5428@gmail.com>
2022-08-13 19:29:23 +0900

    feat: 티켓 아이콘 표시 구현

--- src/js/controller/handlePaymentSubmit.js
+++ src/js/controller/handlePaymentSubmit.js
@@ -1,17 +1,30 @@
 import { LOTTO } from "../constants/constants.js";
+import { ticket } from "../model/model.js";
 import { $ } from "../util/DOM.js";
-import { buyTicketMessage } from "../view/buyTicketMessage.js";
+import {
+  buyTicketMessage,
+  ticketIconTemplate,
+} from "../view/buyTicketMessage.js";
 
 export const handlePaymentSubmit = (event) => {
   event.preventDefault();
 
+  buyTicket();
   paintBuyTicketMessage();
+  paintTicketIcon();
+};
+
+const buyTicket = () => {
+  const $payment = $("#payment-input").value;
+  ticket.count = $payment / LOTTO.TICKET_PRICE;
 };
 
 const paintBuyTicketMessage = () => {
-  const $payment = $("#payment-input").value;
   const $buyTicketMessage = $("#buy-ticket-message");
+  $buyTicketMessage.textContent = buyTicketMessage(ticket.count);
+};
 
-  const ticketCount = $payment / LOTTO.TICKET_PRICE;
-  $buyTicketMessage.textContent = buyTicketMessage(ticketCount);
+const paintTicketIcon = () => {
+  const $ticketIcon = $("#ticket-icon");
+  $ticketIcon.innerHTML = ticketIconTemplate.repeat(ticket.count);
 };
---
+++ src/js/model/model.js
@@ -0,0 +1,3 @@
+export const ticket = {
+  count: null,
+};
