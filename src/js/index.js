3953e7f3737132bf9c373d01f6feba59dfd6d783
jsh3418 <albert5428@gmail.com>
2022-08-17 11:12:06 +0900

    refactor: modal 관련 함수 resultModal.js 파일로 분리

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,14 @@
 import { $ } from "./util/DOM.js";
 import { handlePaymentSubmit } from "./controller/handlePaymentSubmit.js";
 import { handleNumbersToggleButton } from "./controller/handleNumbersToggleButton.js";
-
-const $showResultButton = $(".open-result-modal-button");
-const $modalClose = $(".modal-close");
-const $modal = $(".modal");
-const $lottoNumbersToggleButton = $(".lotto-numbers-toggle-button");
-const $paymentForm = $("#payment-form");
-
-const onModalShow = () => {
-  $modal.classList.add("open");
-};
-
-const onModalClose = () => {
-  $modal.classList.remove("open");
-};
+import { onModalClose, onModalShow } from "./controller/resultModal.js";
 
 const initEventListener = () => {
+  const $showResultButton = $(".open-result-modal-button");
+  const $modalClose = $(".modal-close");
+  const $lottoNumbersToggleButton = $(".lotto-numbers-toggle-button");
+  const $paymentForm = $("#payment-form");
+
   $showResultButton.addEventListener("click", onModalShow);
   $modalClose.addEventListener("click", onModalClose);
   $paymentForm.addEventListener("submit", handlePaymentSubmit);
